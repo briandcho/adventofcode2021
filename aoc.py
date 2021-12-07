3779320207c054250b295157c83b446f82d05365
Brian Cho <bcho@trusted-technologies.com>
2021-12-06 16:31:01 -0800

    chore: refactor day 1

--- aoc.py
+++ aoc.py
@@ -2,21 +2,11 @@ from typing import Callable
 
 
 def count_increases(sweep_data: list[int]) -> int:
-    n_increases = 0
-    for i in range(1, len(sweep_data)):
-        if sweep_data[i - 1] < sweep_data[i]:
-            n_increases += 1
-    return n_increases
+    return sum(sweep_data[i - 1] < sweep_data[i] for i in range(1, len(sweep_data)))
 
 
 def count_window_increases(sweep_data: list[int]) -> int:
-    n_increases = 0
-    for i in range(1, len(sweep_data)):
-        last_window = sweep_data[i - 1 : i + 2]
-        window = sweep_data[i : i + 3]
-        if sum(last_window) < sum(window):
-            n_increases += 1
-    return n_increases
+    return sum(sweep_data[i - 3] < sweep_data[i] for i in range(3, len(sweep_data)))
 
 
 def navigate_no_aim(planned_course: list[str]) -> int:
